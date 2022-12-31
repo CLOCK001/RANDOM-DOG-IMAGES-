2bf2742a67873bb9f24cfaaa9722bbd3ed9ae9ad
CLOCK001 <113068953+CLOCK001@users.noreply.github.com>
2022-12-31 12:43:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//api website
+let dogAPI = 'https://place.dog/300/200'
+
+//set image as the picture in the dog-api
+document.getElementById("img").src= dogAPI
+
+//function to chage picture of dog
+function dog(){
+    //reload the page
+    location.reload();
+}
